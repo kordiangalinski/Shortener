5ef5dc0549d5c5e8cecdd49e2180c8787f3e65fc
Kordian Galinski <31996030+kordiangalinski@users.noreply.github.com>
2023-05-24 11:28:43 +0200

    tests

---
+++ src/test/java/net/kordian/shortener/ShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.kordian.shortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
