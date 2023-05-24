04e2b5fc669fc4f4858049c200a8db5e9aa109f8
Kordian Galinski <31996030+kordiangalinski@users.noreply.github.com>
2023-05-24 11:29:26 +0200

    services and repository

---
+++ src/main/java/net/kordian/shortener/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package net.kordian.shortener.repositories;
+
+import net.kordian.shortener.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    // You can add custom query methods here if needed
+}
---
+++ src/main/java/net/kordian/shortener/services/UserService.java
@@ -0,0 +1,50 @@
+package net.kordian.shortener.services;
+
+import net.kordian.shortener.entities.Gender;
+import net.kordian.shortener.entities.User;
+import net.kordian.shortener.exceptions.UserNotFoundException;
+import net.kordian.shortener.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public User updateUser(User user) {
+        if (userRepository.existsById(user.getId())) {
+            return userRepository.save(user);
+        }
+        return null;
+    }
+
+    public void deleteUser(Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException("User not found with ID: " + id));
+
+        // Perform deletion logic
+        userRepository.delete(user);
+    }
+}
